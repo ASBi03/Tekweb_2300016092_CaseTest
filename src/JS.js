4a7526055889f5b7c6285b3c360c17b3df8d609f
ASBi03 <2300016092@webmail.uad.ac.id>
2024-11-03 20:48:43 +0700

    Case Test

---
+++ src/JS.js
@@ -0,0 +1,5 @@
+//mobile
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.toggle('hidden');
+});
