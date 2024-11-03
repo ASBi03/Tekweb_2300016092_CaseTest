8a6909b92b2df0f81be43129536efc053bbf6dcc
ASBi03 <2300016092@webmail.uad.ac.id>
2024-11-03 17:20:03 +0700

    CaseTest

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
